0e595c05ce4cdd9867cffd27a528f85a5cf1d09e
hamzaramzanali <hamzaramzanali1@gmail.com>
2023-05-10 16:32:24 -0500

    initial commit

---
+++ src/main/java/com/technews/TechnewsJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.technews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnewsJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnewsJavaApiApplication.class, args);
+	}
+
+}
